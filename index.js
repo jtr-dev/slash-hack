619f2f0fc8d3fdaea697b9ef1a6cdba1189862dd
Felix Rilling <contact@f-rilling.com>
2017-04-18 10:32:19 +0200

    updated to di-ngy

--- index.js
+++ index.js
@@ -1,60 +1,15 @@
 "use strict";
 
-const fs = require("fs");
-const Log = require("log");
-const Clingy = require("cli-ngy");
-const Discord = require("discord.js");
-
-const cfg = require("./config.json");
-const strings = require("./lib/strings");
+const Dingy = require("di-ngy");
+const config = require("./config.json");
 const commands = require("./lib/commands");
-const assetsDir = require("./lib/constants").assetsDir;
-const executeCommand = require("./lib/bot/executeCommand");
-
-const logStream = fs.createWriteStream("./data/bot.log");
-const log = new Log("debug", logStream);
-const cli = new Clingy(commands);
-const bot = new Discord.Client();
-
-//Setup bot data storage
-bot._instance = {
-    log,
-    cli,
-    voice: {
-        playing: false,
-        channel: null,
-        connection: null,
-        dispatcher: null,
-        assets: fs.readdirSync(assetsDir),
-    },
-    tags: new Map()
+const onInit = function (app) {
+     console.log(app.data);
+};
+const onConnect = function () {
+    console.log("Bot is running");
 };
 
-//Inits onMessage function
-bot.on("message", msg => {
-    /**
-     * Command proccessor
-     */
-    if (!msg.author.bot && msg.content.substr(0, cfg.prefix.length) === cfg.prefix) {
-        const commandResult = executeCommand(bot, msg);
+const bot = new Dingy(config, commands, {}, { onInit, onConnect });
 
-        if (commandResult.type === "text") {
-            msg.channel.sendMessage(commandResult.content);
-        } else if (commandResult.type === "code" || commandResult.type === "error") {
-            msg.channel.sendCode("text", commandResult.content);
-        } else {
-            throw new Error("Malformed result", commandResult);
-        }
-    }
-
-});
-
-//Start bot when files are loaded
-bot.login(cfg.token)
-    .then(() => {
-        bot.user.setGame(strings.botStatus);
-        log.info(`${cfg.name} is running...`);
-    })
-    .catch(err => {
-        log.error("An error occured connecting to the discord API", err);
-    });
+bot.connect();
