016945f929ad902fc0b00845b9ee73d285861b84
Felix Rilling <contact@f-rilling.com>
2017-04-21 15:03:14 +0200

    integrated stats

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const Dingy = require("di-ngy");
 const config = require("./config.json");
 const commands = require("./lib/commands");
 const onMessage = require("./lib/events/onMessage");
-const onConnect = function (app) {
-    console.log("Bot is running");
-    console.log(app.storage);
-};
+const onConnect = require("./lib/events/onConnect");
 
 const bot = new Dingy(config, commands, {}, {
     onMessage,
