39b94f66e40c694f1dc949c0a08b00d369bff9ac
Felix Rilling <contact@f-rilling.com>
2017-04-22 15:36:33 +0200

    added watering module

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const Dingy = require("di-ngy");
 const config = require("./config.json");
+const strings = require("./strings.json");
+
 const commands = require("./lib/commands");
 const onMessage = require("./lib/events/onMessage");
 const onConnect = require("./lib/events/onConnect");
 
-const bot = new Dingy(config, commands, {}, {
+const bot = new Dingy(config, commands, strings, {
     onMessage,
     onConnect
 });
