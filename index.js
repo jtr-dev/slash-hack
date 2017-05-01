87ea1402a8466fefe8993845de8e830f11dcf355
Felix Rilling <contact@f-rilling.com>
2017-05-01 21:44:16 +0200

    moved api key to env vars

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 "use strict";
 
 const Dingy = require("di-ngy");
-const config = require("./config.json");
-const strings = require("./strings.json");
-
 const commands = require("./lib/commands");
 const onInit = require("./lib/events/onInit");
 const onConnect = require("./lib/events/onConnect");
 const onMessage = require("./lib/events/onMessage");
+
+const strings = {
+    currentlyPlaying: "with leafes",
+    separator: "----------------",
+    errorPermission: "You don't have permissions to access this command",
+    errorInternal: "Internal error",
+    errorUnknownCommand: "Unkown command",
+    errorMissingArgument: "Missing argument",
+    errorTooLong: "The output was to long to print",
+    infoSimilar: "Did you mean",
+    infoEmpty: "Empty message",
+    infoInternal: "Internal error"
+};
+const config = {
+    name: "lisa-bot",
+    prefix: "$",
+    token: process.env.DISCORD_KEY,
+    adminIds: [
+        "128985967875850240"
+    ],
+    website: "https://github.com/FelixRilling/lisa-bot",
+    files: {
+        data: {
+            dir: "./data/",
+            log: "bot",
+            storage: [
+                "lisa",
+                "tag_storage",
+                "tictactoe"
+            ]
+        },
+        assets: "./data/assets/"
+    },
+    options: {
+        enableDefaultCommands: true,
+        commandsAreCaseSensitive: false,
+        answerToMissingComman: false,
+        answerToMissingArgs: false,
+        answerToMissingPerms: false
+    }
+};
 
 const bot = new Dingy(config, commands, strings, {
     onInit,
